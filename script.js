e987ffab259c28e4a9d8ddbec51a32915c5c9695
Ronja <roba0001@stud.kea.dk>
2024-10-24 08:48:45 +0000

    fungerer nu

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const submitButton = document.querySelector("button");
 const taskInput = document.querySelector("#task");
 const taskList = document.querySelector("#taskList");
+const doneTaskList = document.querySelector("#doneTaskList");
 
 const taskListFromLS = localStorage.getItem("taskList");
 
 let toDoModel;
-let doneModel;
+let doneModel = [];
 
 if (taskListFromLS === null) {
-  toDoModel = ["Task 1", "Task 2", "Task 3"];
+  toDoModel = ["Isert your first task"];
 } else {
   toDoModel = JSON.parse(taskListFromLS);
 }
@@ -17,6 +18,7 @@ init();
 
 function init() {
   submitButton.addEventListener("click", buttonClicked);
+
   updateList();
 }
 
@@ -26,7 +28,6 @@ function buttonClicked() {
 }
 
 function addTaskToList(task) {
-  console.log(task);
   toDoModel.push(task);
   updateLocalStorage();
 }
@@ -36,17 +37,36 @@ function removeTaskFromList(id) {
   updateLocalStorage();
 }
 
+function addTaskToDone(task) {
+  doneModel.push(task);
+  updateLocalStorage();
+  updateList();
+}
+
+function removeTaskFromDone(id) {
+  doneModel.splice(id, 1);
+  updateLocalStorage();
+}
+
 function updateLocalStorage() {
   const inputFelt = document.querySelector("input");
-  console.log("updateLocalStorage", toDoModel);
+  console.log("toDoModel:", toDoModel);
+  console.log("doneModel:", doneModel);
   localStorage.setItem("taskList", JSON.stringify(toDoModel));
 }
 
 function updateList() {
   taskList.innerHTML = "";
+  doneTaskList.innerHTML = "";
+
   toDoModel.forEach((each, i) => {
-    taskList.innerHTML += `<div><input type="checkbox" class="clickTask" data-id="${i}"> 
+    taskList.innerHTML += `<div class="clickTask"><input data-filter="toDo" type="checkbox" data-id="${i}"> 
     <label for="${i}">${each}</label></div>`;
+  });
+
+  doneModel.forEach((each, i) => {
+    doneTaskList.innerHTML += `<div class="clickTask"><input data-filter="done" type="checkbox" data-id="${i}">
+          <label for="${i}">${each}</label></div>`;
   });
 
   document.querySelectorAll(".clickTask").forEach((each) => {
@@ -55,6 +75,20 @@ function updateList() {
 }
 
 function clickTask(evt) {
-  removeTaskFromList(evt.target.dataset.id);
+  const action = evt.target.dataset.filter; // konstant for den trykkede button's (evt target's) datafilter
+  let text = evt.currentTarget.querySelector("label").textContent;
+
+  console.log("text", text);
+
+  if (action === "toDo") {
+    removeTaskFromList(evt.target.dataset.id);
+    addTaskToDone(text);
+  } else if (action === "done") {
+    addTaskToList(text);
+    removeTaskFromDone(evt.target.dataset.id);
+  }
+
+  console.log(action);
+
   updateList();
 }
